2f4a24b72fe01e479c32e5b1edc7b0e4a2bc987f
samsonchan <schan25@students.cs.ubc.ca>
2025-03-15 09:50:26 -0700

    config-real

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyARY4AiDSX248BygFFS-mMkHlk3HlZDe04",
   authDomain: "jobbox-68180.firebaseapp.com",
@@ -16,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-CKJNC55007"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+export const auth =getAuth(app);
