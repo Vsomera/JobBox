6efa93be23f91fd9f6bac0564e311c7ef736478f
Vincent Wong <vincentwong5609@gmail.com>
2025-03-15 11:27:14 -0700

    added search bar to explore

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -1,9 +1,37 @@
 import { Header } from '../../components/Header/Header';
+import styles from './explore.module.css';
 
 export const Explore = () => {
 	return (
 		<>
 			<Header />
+			<div id={styles.explore_root}>
+				<main>
+					<h1>Search for Jobs</h1>
+					<div id={styles.search}>
+						<img
+							src='../../../public/search_bl.png'
+							alt=''
+							className={styles.search_icons}
+						/>
+						<input
+							type='text'
+							placeholder='Add a job title'
+							className={styles.search_boxes}
+						/>
+						<img
+							src='../../../public/map_pin.png'
+							alt=''
+							className={styles.search_icons}
+						/>
+						<input
+							type='text'
+							placeholder='Add a location'
+							className={styles.search_boxes}
+						/>
+					</div>
+				</main>
+			</div>
 		</>
 	);
 };
