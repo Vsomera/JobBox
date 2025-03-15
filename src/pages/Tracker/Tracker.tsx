c3ada2b55e4ffe1e5eb227d2ccaea4f2a9a0d3ca
Vsomera <vsomera77@outlook.com>
2025-03-15 10:51:45 -0700

    waves and firebase setup

--- src/pages/Tracker/Tracker.tsx
+++ src/pages/Tracker/Tracker.tsx
@@ -1,3 +1,15 @@
+import { Wave3 } from "../../assets/waveL";
+
 export const Tracker = () => {
-	return <>Tracker Page</>;
+	return (<>
+		<div className="w-vw h-screen flex justify-between">
+			<div>
+				<Wave3 width="w-full" height="h-full" />
+			</div>
+			
+			<div className="rotate-180">
+				<Wave3 width="w-full" height="h-full" />
+			</div>
+		</div>
+	</>);
 };
